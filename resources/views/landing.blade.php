dc416c9aa583105cebf2d620b6cdd53a24c0d345
erida34 <40295590+erida34@users.noreply.github.com>
2026-08-08 22:47:16 +0300

    верификация в яндексе и гугл. а также метрика

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -39,7 +39,8 @@
   <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any" />
 
   <link rel="stylesheet" href="{{ asset('css/main.css') }}?v={{ filemtime(public_path('css/main.css')) }}" />
-
+  <meta name="yandex-verification" content="b8bd60e37c3c4eca" />
+  <meta name="google-site-verification" content="Ru9BG4bHA3ezcDdihkyDW-DsJbdYOiVVCiQcJ9GAThs" />
   <!-- Schema.org Structured Data (JSON-LD) -->
   <script type="application/ld+json">
   {
@@ -131,6 +132,19 @@
     ]
   }
   </script>
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript">
+      (function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+      })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=111424038', 'ym');
+
+      ym(111424038, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/111424038" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  <!-- /Yandex.Metrika counter -->
 </head>
 <body>
   <header class="header">
